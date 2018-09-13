aa9641489cf446de80585c95cace4dd0b7160b1a
Karthik Nayak <nayakkarthik02@gmail.com>
2018-09-12 22:39:25 -0400

    Add files via upload

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,27 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                if (result == DialogResult.OK) // Test result.
+                {
+                    Console.WriteLine("bustin thru the roof like a AP", file);
+                }
+                Console.WriteLine(result); // <-- For debugging use.
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
     }
 }
